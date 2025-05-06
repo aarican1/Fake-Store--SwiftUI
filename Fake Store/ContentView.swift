45570388618db3dfbc05061c36e0ed261ad1a0c2
Arican <93190017+aarican1@users.noreply.github.com>
2025-05-06 17:00:04 +0300

    Initial Commit

---
+++ Fake Store/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fake Store
+//
+//  Created by Abdurrahman Arıcan on 6.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fake Store/Fake_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fake_StoreApp.swift
+//  Fake Store
+//
+//  Created by Abdurrahman Arıcan on 6.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Fake_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
